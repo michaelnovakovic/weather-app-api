ddc6a7d495750a5b4f1a7f5bc61b27e68a082fb3
Michael Novakovic <michael076446@hotmail.com>
2022-09-14 19:02:16 -0400

    Changes were made

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let weather = {
-    "apiKey": "f447820917320f2e8f7c825bec1d282d",
-    fetchWeather: function() {
+    apiKey: "f447820917320f2e8f7c825bec1d282d",
+    fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=f447820917320f2e8f7c825bec1d282d"
-        ).then((response) => response.json())
-        .then((data) => console.log(data));
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
     },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.com/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = humidity + "%";
+        document.querySelector(".wind").innerText = speed + " km/hr";
+    }
 };
